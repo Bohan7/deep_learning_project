cafe0c19bd105e8fcd59efba5e4f1522e6e8d06d
Bohan Wang <bohan.wang@epfl.ch>
2021-05-25 20:00:42 +0200

    updated by Bohan

--- project1/model.py
+++ project1/model.py
@@ -11,15 +11,15 @@ from torch.nn import functional as F
 
 # No weight sharing. No auxiliary loss.
 class CNN(nn.Module):
-     def __init__(self, in_channels=2, out_channels_1=32, out_channels_2=64, output_fc=256, use_bn=True):
+     def __init__(self, in_channels=2, out_channels_1=32, out_channels_2=64, output_fc=30, use_bn=True):
         super(CNN,self).__init__()
         
         self.use_bn = use_bn
-        self.conv1 = nn.Conv2d(in_channels, out_channels_1, kernel_size=3)
-        self.conv2 = nn.Conv2d(out_channels_1, out_channels_2, kernel_size=3)
+        self.conv1 = nn.Conv2d(in_channels, out_channels_1, kernel_size=5)
+        self.conv2 = nn.Conv2d(out_channels_1, out_channels_2, kernel_size=5)
         self.active = nn.ReLU(inplace=True)
         self.pool = nn.MaxPool2d((2,2))
-        self.fc1 = nn.Linear(out_channels_2*5*5, output_fc)
+        self.fc1 = nn.Linear(out_channels_2*3*3, output_fc)
         self.fc2 = nn.Linear(output_fc, 2)
 
         
@@ -43,10 +43,12 @@ class CNN(nn.Module):
         return out
 
 # Only weight sharing. 
-class CNN_WS(nn.Module): 
-    def __init__(self, in_channels=2, out_channels_1=32, out_channels_2=64, output_fc1=50, output_fc2=25, use_bn=True):
-        super(CNN_WS,self).__init__()
-        
+class Siamese_net(nn.Module): 
+    def __init__(self, in_channels=2, out_channels_1=32, out_channels_2=64, output_fc1=50, output_fc2=25, use_bn=True, version=1):
+        super(Siamese_net,self).__init__()
+        self.version = version
+        self.dropout1 = nn.Dropout(p=0.25)
+        self.dropout2 = nn.Dropout(p=0.5)
         self.use_bn = use_bn
         self.conv1 = nn.Conv2d(in_channels // 2, out_channels_1, kernel_size=3)
         self.conv2 = nn.Conv2d(out_channels_1, out_channels_2, kernel_size=3)
@@ -54,6 +56,7 @@ class CNN_WS(nn.Module):
         self.pool = nn.MaxPool2d((2,2))
         self.fc1 = nn.Linear(out_channels_2*5*5, output_fc1)
         self.fc2 = nn.Linear(output_fc1, 10)
+        #self.fc3 = nn.Linear(20, 2)
         self.fc3 = nn.Linear(20, output_fc2)
         self.fc4 = nn.Linear(output_fc2, 2)
 
@@ -84,9 +87,16 @@ class CNN_WS(nn.Module):
         x1_class = self.one_branch(x1)
         x2_class = self.one_branch(x2)
         
-        out = torch.cat((x1_class, x2_class), dim=1)
-        out = self.active(self.fc3(out))
-        out = self.fc4(out)
+        if self.version == 1:
+          out = torch.cat((x1_class, x2_class), dim=1)
+          out = self.active(self.fc3(out))
+          out = self.fc4(out)
+        elif self.version == 2:
+          _, predicted_digit1 = torch.max(x1_class, 1)
+          _, predicted_digit2 = torch.max(x2_class, 1)
+          out = (predicted_digit1 <= predicted_digit2).float()
+          #print(out.size())
+        #out = self.fc3(out)
         return out, (x1_class, x2_class)
 
 # No weight sharing. No auxiliary loss.
@@ -98,8 +108,8 @@ class Resnetblock(nn.Module):
         self.active = nn.ReLU(inplace=True)
         self.conv1 = nn.Conv2d(in_channels, out_channels_1, kernel_size=self.kernel_size, padding=(self.kernel_size-1)//2)
         self.conv2 = nn.Conv2d(out_channels_1, out_channels_2, kernel_size=self.kernel_size, padding=(self.kernel_size-1)//2)
-        self.pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc1 = nn.Linear(out_channels_2, output_fc)
+        self.pool = nn.MaxPool2d((2,2))
+        self.fc1 = nn.Linear(out_channels_2*7*7, output_fc)
         self.fc2 = nn.Linear(output_fc, 2)
         self.shortcut = nn.Sequential(
                 nn.Conv2d(in_channels, out_channels_2, kernel_size=1),
@@ -128,57 +138,57 @@ class Resnetblock(nn.Module):
         
         return out
 
-
-# Only weight sharing. 
-class Resnet_WS(nn.Module): 
-    def __init__(self, in_channels=2, out_channels_1=32, out_channels_2=64, output_fc=256, use_bn=True):
-        super(CNN_WS,self).__init__()
-        
+# No weight sharing. No auxiliary loss.
+class Resnetblock_WS(nn.Module):
+     def __init__(self, in_channels=2, out_channels_1=32, out_channels_2=32, output_fc=256, kernel_size=3, use_bn=True):
+        super(Resnetblock_WS,self).__init__()
+        self.kernel_size = kernel_size
         self.use_bn = use_bn
-        self.conv1 = nn.Conv2d(in_channels // 2, out_channels_1, kernel_size=3)
-        self.conv2 = nn.Conv2d(out_channels_1, out_channels_2, kernel_size=3)
         self.active = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_channels//2, out_channels_1, kernel_size=self.kernel_size, padding=(self.kernel_size-1)//2)
+        self.conv2 = nn.Conv2d(out_channels_1, out_channels_2, kernel_size=self.kernel_size, padding=(self.kernel_size-1)//2)
         self.pool = nn.MaxPool2d((2,2))
-        self.fc1 = nn.Linear(out_channels_2*5*5, output_fc)
+        self.fc1 = nn.Linear(out_channels_2*7*7, output_fc)
         self.fc2 = nn.Linear(output_fc, 10)
         self.fc3 = nn.Linear(20, 2)
 
-        
+        self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels//2, out_channels_2, kernel_size=1),
+                nn.BatchNorm2d(out_channels_2),
+                )
+
         if self.use_bn:
             self.bn1 = nn.BatchNorm2d(out_channels_1)
             self.bn2 = nn.BatchNorm2d(out_channels_2)
         else:
             self.bn1 = lambda x: x
             self.bn2 = lambda x: x
-   
-    def one_branch_feature(self, x):
-        x = self.bn1(self.active(self.conv1(x)))
-        x = self.bn2(self.active(self.conv2(x)))
-        x = self.pool(x)
+             
+     def one_branch(self, x):
+        out = self.bn1(self.conv1(x))
+        out = self.active(out)
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = self.active(out)
+        out = self.pool(out)
+        out = torch.flatten(out, 1)
+        out = self.active(self.fc1(out))
+        out = self.fc2(out)
         
-        return x
-    
-    def one_branch_classify(self, x):
-        x = x.view(x.size()[0], -1)
-        x = self.active(self.fc1(x))
-        x = self.fc2(x)
+        return out
+
         
-        return x
-        
-        
-    def forward(self, x):
+     def forward(self, x):
         x1, x2 = x[:, 0], x[:, 1]
         x1 = x1.reshape(-1, 1, 14, 14)
         x2 = x2.reshape(-1, 1, 14, 14)
         
-        x1_feature = self.one_branch_feature(x1)
-        x2_feature = self.one_branch_feature(x2)
-        x1_class = self.one_branch_classify(x1_feature)
-        x2_class = self.one_branch_classify(x2_feature)
+        x1_class = self.one_branch(x1)
+        x2_class = self.one_branch(x2)
         
         out = torch.cat((x1_class, x2_class), dim=1)
         out = self.fc3(out)
-        return out, (x1_class, x2_class)    
-        
 
-        
+        return out, (x1_class, x2_class)
+
+
--- project1/train.py
+++ project1/train.py
@@ -9,7 +9,8 @@ import torch
 from torch import nn
 from torch import optim
 from torch.nn import functional as F
-from model import CNN, CNN_WS, Resnetblock
+from model import CNN, Siamese_net, Resnetblock, Resnetblock_WS
+from visualise import plot_train_loss_test_accuracy
 
 import matplotlib.pyplot as plt
 from dlc_practical_prologue import generate_pair_sets
@@ -18,7 +19,10 @@ def standardize(train_data, test_data):
     mean, std = train_data.mean(), train_data.std()
     return (train_data - mean) / std, (test_data - mean) / std
 
-def train_CNN(model, train_input, train_target, mini_batch_size, criterion, optimizer, nb_epochs):
+def train_CNN(model, train_input, train_target, test_input, test_target, mini_batch_size, criterion, optimizer, nb_epochs, verbose=True):
+  loss_values = []
+  acc_values = []
+
   for e in range(nb_epochs):
     train_loss = 0
     for b in range(0, train_input.size(0), mini_batch_size):
@@ -29,40 +33,66 @@ def train_CNN(model, train_input, train_target, mini_batch_size, criterion, opti
       optimizer.step()
 
       train_loss += loss.item()
-    
-    print('epoch {}: loss: {}'.format(e, train_loss))
 
-def train_CNN_WS(model, train_input, train_target, mini_batch_size, criterion, optimizer, nb_epochs):
+    
+    
+    if verbose:
+      acc = test_CNN(model, test_input, test_target)
+      print('epoch {}:   loss: {}    acc: {}'.format(e, train_loss, acc))
+      loss_values.append(train_loss)
+      acc_values.append(acc.item())
+    
+    if (e > 5) and ((loss_values[-1] - loss_values[-2]) > 10.0):
+      loss_values = loss_values[:-1] + [loss_values[-2]] * (nb_epochs-len(loss_values[:-1]))
+      acc_values = acc_values[:-1] + [acc[-2]] * (nb_epochs-len(acc_values[:-1]))
+      break
+    
+
+  return loss_values, acc_values
+
+def train_Siamese_net(model, train_input, train_target, train_classes, test_input, test_target, test_classes, mini_batch_size, criterion, optimizer, nb_epochs, loss_weight, version=1, verbose=True):
+  loss_values = []
+  acc_values = []
+
   for e in range(nb_epochs):
     train_loss = 0
     for b in range(0, train_input.size(0), mini_batch_size):
       model.zero_grad()
       output, (pred_class_1, pred_class_2) = model(train_input.narrow(0, b, mini_batch_size))
-      loss = criterion(output, train_target.narrow(0, b, mini_batch_size))
-      loss.backward()
-      optimizer.step()
 
-      train_loss += loss.item()
-    
-    print('epoch {}: loss: {}'.format(e, train_loss))
-
-def train_CNN_WS_AL(model, train_input, train_target, train_classes, mini_batch_size, criterion, optimizer, nb_epochs, loss_weight):
-  for e in range(nb_epochs):
-    train_loss = 0
-    for b in range(0, train_input.size(0), mini_batch_size):
-      model.zero_grad()
-      output, (pred_class_1, pred_class_2) = model(train_input.narrow(0, b, mini_batch_size))
-      loss_bool = criterion(output, train_target.narrow(0, b, mini_batch_size))
       loss_class_1 = criterion(pred_class_1, train_classes.narrow(0, b, mini_batch_size)[:,0])
       loss_class_2 = criterion(pred_class_2, train_classes.narrow(0, b, mini_batch_size)[:,1])
       
-      total_loss = loss_weight[0] * loss_bool + loss_weight[1] * (loss_class_1+loss_class_2)
+      if version == 1:
+        loss_bool = criterion(output, train_target.narrow(0, b, mini_batch_size))
+        total_loss = loss_weight[0] * loss_bool + loss_weight[1] * (loss_class_1+loss_class_2)
+      elif version == 2:
+        total_loss = loss_weight[1] * (loss_class_1+loss_class_2)
+
       total_loss.backward()
       optimizer.step()
 
       train_loss += total_loss.item()
+
+      
     
-    print('epoch {}: loss: {}'.format(e, train_loss))
+    if verbose and (version==1):
+      acc = test_Siamese_net(model, test_input, test_target, version=1)
+      print('epoch: {}   loss: {}   acc: {}'.format(e, train_loss, acc))
+    elif verbose and (version==2):
+      acc = test_Siamese_net(model, test_input, test_target, version=2)
+      print('epoch: {}   loss: {}   acc: {}'.format(e, train_loss, acc))
+    
+    loss_values.append(train_loss)
+    acc_values.append(acc.item())
+
+    if (e > 5) and ((loss_values[-1] - loss_values[-2]) > 10.0):
+      loss_values = loss_values[:-1] + [loss_values[-2]] * (nb_epochs-len(loss_values[:-1]))
+      acc_values = acc_values[:-1] + [acc_values[-2]] * (nb_epochs-len(acc_values[:-1]))
+      break
+    
+  return loss_values, acc_values
+
 
 
 def test_CNN(model, test_input, test_target):  
@@ -75,13 +105,16 @@ def test_CNN(model, test_input, test_target):
 
     return accuracy
 
-def test_CNN_WS(model, test_input, test_target):  
+def test_Siamese_net(model, test_input, test_target, version=1):  
     model.eval()
     with torch.no_grad():
         pred, (pred_class_1, pred_class_2) = model(test_input)
     
-    _, predicted = pred.max(1)
-    accuracy = (predicted == test_target).float().mean()
+    if version == 1:
+      _, predicted = pred.max(1)
+      accuracy = (predicted == test_target).float().mean()
+    elif version == 2:
+      accuracy = (pred == test_target).float().mean()
 
     return accuracy
 
@@ -90,42 +123,92 @@ def main():
     device = torch.device('cuda' if cuda else 'cpu')
 
     N = 1000
-    n_run = 20
+    n_run = 15
     test_accuracy = torch.zeros(n_run)
     cross_entropy = nn.CrossEntropyLoss()
+
+    model_names = ['CNN', 'Siamese_version1', 'Siamese_version1_auxiliary_loss', 'Siamese_version2', 'Resnet_block', 'Resnet_block_weight_share', 'Resnet_block_weight_share_aux']
+
+    models_loss = []
+    models_acc = []
+    for name in model_names:
+      runs_loss = []
+      runs_accuracy = []
+      for i in range(n_run):
+        seed = random.randint(1, 10000)
+        random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+
+        train_input, train_class, train_digit, test_input, test_class, test_digit = generate_pair_sets(N)
+
+        if torch.cuda.is_available():
+          train_input, train_class, train_digit = train_input.to(device), train_class.to(device), train_digit.to(device)
+          test_input, test_class, test_digit = test_input.to(device), test_class.to(device), test_digit.to(device)
     
-    for i in range(n_run):
-      seed = random.randint(1, 10000)
-      random.seed(seed)
-      torch.manual_seed(seed)
-      torch.cuda.manual_seed_all(seed)
+        train_input, test_input = standardize(train_input, test_input)
+
+        print('\nrun {}!'.format(i))
+        
+        if 'CNN' is name:
+          print('CNN')
+          model = CNN(in_channels=2, out_channels_1=32, out_channels_2=64, output_fc=25, use_bn=True).to(device)
+          optimizer = optim.SGD(model.parameters(), lr = 1e-1)
+          train_loss, test_acc = train_CNN(model, train_input, train_class, test_input, test_class, 100, cross_entropy, optimizer, 25)
+        elif 'Siamese_version1' is name:
+          print(name)
+          model = Siamese_net(in_channels=2, out_channels_1=16, out_channels_2=32, output_fc1=50, output_fc2=25, use_bn=True, version=1).to(device)
+          optimizer = optim.SGD(model.parameters(), lr = 1e-1)
+          train_loss, test_acc = train_Siamese_net(model, train_input, train_class, train_digit, test_input, test_class, test_digit, 100, cross_entropy, optimizer, 25, [1,0], version=1)
+        elif 'Siamese_version1_auxiliary_loss' is name:
+          print(name)
+          model = Siamese_net(in_channels=2, out_channels_1=16, out_channels_2=32, output_fc1=50, output_fc2=25, use_bn=True, version=1).to(device)
+          optimizer = optim.SGD(model.parameters(), lr = 1e-1)
+          train_loss, test_acc = train_Siamese_net(model, train_input, train_class, train_digit, test_input, test_class, test_digit, 100, cross_entropy, optimizer, 25, [1,1], version=1)
+         
+        elif 'Siamese_version2' is name:
+          print(name)
+          model = Siamese_net(in_channels=2, out_channels_1=16, out_channels_2=32, output_fc1=50, output_fc2=25, use_bn=True, version=2).to(device)
+          optimizer = optim.SGD(model.parameters(), lr = 1e-1)
+          train_loss, test_acc = train_Siamese_net(model, train_input, train_class, train_digit, test_input, test_class, test_digit, 100, cross_entropy, optimizer, 25, [0, 1], version=2)
+          
+        elif 'Resnet_block' is name:
+          print(name)
+          model = Resnetblock(in_channels=2, out_channels_1=16, out_channels_2=16, output_fc=80, kernel_size=3, use_bn=True).to(device)
+          optimizer = optim.SGD(model.parameters(), lr = 1e-1)
+          train_loss, test_acc = train_CNN(model, train_input, train_class, test_input, test_class, 100, cross_entropy, optimizer, 25)
+        elif 'Resnet_block_weight_share' is name:
+          print(name)
+          model = Resnetblock_WS(in_channels=2, out_channels_1=16, out_channels_2=16, output_fc=80, kernel_size=3, use_bn=True).to(device)
+          optimizer = optim.SGD(model.parameters(), lr = 1e-1)
+          train_loss, test_acc = train_Siamese_net(model, train_input, train_class, train_digit, test_input, test_class, test_digit, 100, cross_entropy, optimizer, 25, [1,0], version=1)
+         
+        elif 'Resnet_block_weight_share_aux' is name:
+          print(name)
+          model =  Resnetblock_WS(in_channels=2, out_channels_1=16, out_channels_2=16, output_fc=80, kernel_size=3, use_bn=True).to(device)
+          optimizer = optim.SGD(model.parameters(), lr = 1e-1)
+          train_loss, test_acc = train_Siamese_net(model, train_input, train_class, train_digit, test_input, test_class, test_digit, 100, cross_entropy, optimizer, 25, [1,1], version=1)
+
+        if i==0:
+          total_params = sum(p.numel() for p in model.parameters())
+          print('There are {} params'.format(total_params))
+        
+        
+        runs_loss.append(train_loss)
+        runs_accuracy.append(test_acc)
+
+      models_loss.append(runs_loss)
+      models_acc.append(runs_accuracy)
       
 
-      model = CNN_WS(in_channels=2, out_channels_1=32, out_channels_2=64, output_fc1=50, output_fc2=25, use_bn=True).to(device)
-      #model = Resnetblock(in_channels=2, out_channels_1=64, out_channels_2=64, output_fc=50, kernel_size=3, use_bn=True)
-      optimizer = optim.SGD(model.parameters(), lr = 1e-1)
+      #mean = test_accuracy.mean().item()
+      #std = test_accuracy.std().item()
+      #print(f'Test accuracy: {mean:.2f} +- {std:.2f}')
+    plot_train_loss_test_accuracy(models_loss, models_acc, model_names)
 
-      if i==0:
-        total_params = sum(p.numel() for p in model.parameters())
-        print('There are {} params'.format(total_params))
 
-      train_input, train_class, train_digit, test_input, test_class, test_digit = generate_pair_sets(N)
-
-      if torch.cuda.is_available():
-        train_input, train_class, train_digit = train_input.to(device), train_class.to(device), train_digit.to(device)
-        test_input, test_class, test_digit = test_input.to(device), test_class.to(device), test_digit.to(device)
     
-      train_input, test_input = standardize(train_input, test_input)
-
-      print('\nrun {}!'.format(i))
-      train_CNN_WS_AL(model, train_input, train_class, train_digit, 100, cross_entropy, optimizer, 25, [1, 1])
-      acc = test_CNN_WS(model, test_input, test_class)
-      test_accuracy[i] = acc
-      print(acc)
-    mean = test_accuracy.mean().item()
-    std = test_accuracy.std().item()
-    print(f'Test accuracy: {mean:.2f} +- {std:.2f}')
-
 if __name__ == '__main__':
     main()
     
